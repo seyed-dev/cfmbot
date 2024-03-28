a175ec869e3e865c7718a5967fe633c68882f42f
seyed <mohamad.khoshnava@gmail.com>
2024-03-28 16:27:40 +0330

    change start keyboard and add callback data per button

--- src/telegram/keyboards.py
+++ src/telegram/keyboards.py
@@ -1,15 +1,14 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-
 def start_keyboard():
     # Create ReplyKeyboardMarkup
     builder = InlineKeyboardBuilder()
-    buttons = [
-        "درباره دوره", "لینک دعوت",
-        "کیف پول", "لیست فیلم ها",
-        "فیلم های من", "زمان کلاس ها",
-    ]
-    for btn in buttons:
-        builder.button(text=btn, callback_data='test')
-    builder.adjust(2,2,1)
+    
+    builder.button(text="درباره دوره", callback_data='about_bootcamp')
+    builder.button(text="لینک دعوت", callback_data='invite_link')
+    builder.button(text="کیف پول", callback_data='wallet')
+    builder.button(text="لیست فیلم ها", callback_data='video_list')
+    builder.button(text="فیلم های من", callback_data='my_videos')
+    builder.button(text="زمان کلاس ها", callback_data='class_time')
+    builder.adjust(2)
     return builder.as_markup()
