b2714826cc4e7b681c07b5d82380fe6ceca65df9
seyed <mohamad.khoshnava@gmail.com>
2024-03-28 16:26:40 +0330

    refactor user model and create index

--- models/mgo_models.py
+++ models/mgo_models.py
@@ -1,8 +1,19 @@
-from mongoengine import Document, StringField, IntField
+from mongoengine import Document, StringField, IntField, BooleanField
 
 class User(Document):
-    name = StringField()
+    telegram_id = IntField(required=True, unique=True)
+    full_name = StringField()
     username = StringField()
     wallet = IntField(min_value=0, default=0)
+    invites = IntField(min_value=0, default=0)
+    is_premium = BooleanField()
+
+    meta = {
+        'collection': 'user',
+        'indexes': [
+            'telegram_id',
+            'username'
+        ]
+    }
 
 
