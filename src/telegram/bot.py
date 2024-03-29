4986ceb9f13c56d0186e6195da20de8351c8af61
seyed <mohamad.khoshnava@gmail.com>
2024-03-29 16:13:58 +0330

    save user in database and check referal code, show wallet balance

--- src/telegram/bot.py
+++ src/telegram/bot.py
@@ -17,28 +17,74 @@ from mongoengine import connect
 import src.telegram.messages as messages
 import src.telegram.keyboards as keyboards
 
+from models.mgo_models import User
+
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 config = {}
+bot = None
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
-    This handler receives messages with `/start` command
-    """
-    if len(message.text.split()) == 1:
-        await message.answer(messages.start, reply_markup=keyboards.start_keyboard())
-        return
-    
-    ـ, query_command = message.text.split()
-    if query_command.startswith("ref_"):
-        if message.from_user.id == int(query_command.split('_')[1]):
-            await message.answer("خودتو دعوت مردک", reply_markup=keyboards.start_keyboard())
-        else:
-            await message.answer("خوش اومدی قشنگم", reply_markup=keyboards.start_keyboard())
+    Handle the /start command from users.
 
+    Args:
+        message (Message): The message object sent by the user.
+
+    Returns:
+        None
+    """
+    # Check if the user exists in the database
+    user = User.objects.filter(telegram_id=message.from_user.id).first()
+    new_user = False
+    # If user doesn't exist, mark as new user and prepare user data
+    if not user:
+        new_user = True
+        user_data = {
+            "telegram_id": message.from_user.id,
+            "username": message.from_user.username,
+            "full_name": message.from_user.full_name,
+            "is_premium": message.from_user.is_premium
+        }
     
+    # Determine the number of arguments in the command
+    num_args = len(message.text.split())
+    
+    # Based on the number of arguments, take appropriate action
+    match num_args:
+        # If only the command is sent (just /start)
+        case 1:
+            # If it's a new user, create a user in the database
+            if new_user:
+                User.objects.create(**user_data)
+        # If command has two arguments (ex: /start ref_111111)
+        case 2:
+            # Split the command into two parts
+            ـ, query_command = message.text.split()
+            if query_command.startswith("ref_"):
+                # Get the inviter user from the referral code
+                inviter = User.objects.filter(telegram_id=int(query_command.split('_')[1])).first()
+                # If it's a new user and inviter exists, update inviter's invites count,
+                # save the new user with inviter's reference, and send a message to inviter
+                if new_user and inviter:
+                    inviter.invites += 1
+                    inviter.save()
+                    user_data["inviter"] = inviter.telegram_id
+                    User.objects.create(**user_data)
+                    await bot.send_message(
+                        chat_id=inviter.telegram_id, 
+                        text=messages.inviter_message.format(
+                            full_name=message.from_user.full_name
+                        )
+                    )
+                # If it's a new user but inviter doesn't exist, just create the user
+                else:
+                    User.objects.create(**user_data)
+
+    # Send the start message with start keyboard to the user
+    await message.answer(messages.start, reply_markup=keyboards.start_keyboard())
 
 
 @dp.callback_query()
@@ -49,7 +95,7 @@ async def handle_callback_query(callback_query: types.CallbackQuery):
 
     match (callback_data):
         case "about_bootcamp":
-            await callback_query.message.answer("about bot camp!")
+            await callback_query.message.answer("about bot camp!", reply_markup=keyboards.start_keyboard())
         case "invite_link":
             await callback_query.message.answer(
                 messages.invite_link.format(
@@ -58,19 +104,20 @@ async def handle_callback_query(callback_query: types.CallbackQuery):
                 )
             )
         case "wallet":
-            await callback_query.message.answer("You clicked the button!")
+            user = User.objects.get(telegram_id=int(callback_query.from_user.id))
+            await callback_query.message.answer(messages.wallet_balance.format(user.wallet), reply_markup=keyboards.start_keyboard())
         case "video_list":
-            await callback_query.message.answer("You clicked the button!")
+            await callback_query.message.answer("You clicked the button!", reply_markup=keyboards.start_keyboard())
         case "my_videos":
-            await callback_query.message.answer("You clicked the button!")
+            await callback_query.message.answer("You clicked the button!", reply_markup=keyboards.start_keyboard())
         case "class_time":
-            await callback_query.message.answer("You clicked the button!")
+            await callback_query.message.answer(messages.class_time, reply_markup=keyboards.start_keyboard())
 
 
 
 async def main() -> None:
     # Load configs from yaml file
-    global config
+    global config, bot
     config = load_config("config.yaml")
     # Connect to db
     connect(
